4b2eeae9e2264885bf884f0293afc0a443897466
solitudeycq <solitudeycq@163.com>
2019-11-24 11:47:42 +0800

    add ignore file

---
+++ src/com/solitudeycq/leetcode/LeetCode237/LeetCode237.java
@@ -0,0 +1,20 @@
+package com.solitudeycq.leetcode.LeetCode237;
+
+/**
+ * Definition for singly-linked list. public class ListNode { int val; ListNode next; ListNode(int x) { val = x; } }
+ */
+public class LeetCode237 {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
---
+++ src/com/solitudeycq/leetcode/LeetCode237/README.md
@@ -0,0 +1,4 @@
+## LeetCode 237
+[LeetCode 237](https://leetcode-cn.com/problems/delete-node-in-a-linked-list/)
+###解题思路
+将下一节点的值复制到当前节点，将当前节点的next设置为下一节点的next。
